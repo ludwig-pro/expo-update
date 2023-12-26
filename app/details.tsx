ebc31ce38ef8bf6d03dc75c7d535a51be0497f3e
ludwig <hello@ludwigvantours.dev>
2023-12-26 13:21:31 +0100

    Initial commit
    Generated by create-expo-stack 2.0.0.

---
+++ app/details.tsx
@@ -0,0 +1,34 @@
+import { useLocalSearchParams } from 'expo-router';
+import { StyleSheet, View, Text } from 'react-native';
+
+export default function Details() {
+  const { name } = useLocalSearchParams();
+  return (
+    <View style={styles.container}>
+      <View style={styles.main}>
+        <Text style={styles.title}>Details</Text>
+        <Text style={styles.subtitle}>Showing details for user {name}.</Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+  },
+  main: {
+    flex: 1,
+    maxWidth: 960,
+    marginHorizontal: 'auto',
+  },
+  title: {
+    fontSize: 64,
+    fontWeight: 'bold',
+  },
+  subtitle: {
+    fontSize: 36,
+    color: '#38434D',
+  },
+});
